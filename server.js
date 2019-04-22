b267d7d6a14ba119372b09f998ed1443add9d0bb
adamjorycromer <adamjorycromer@gmail.com>
2019-04-21 22:32:44 -0600

    added react route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.listen(PORT, function() {
     console.log('Server made successfully. View at: http://localhost:' + PORT);
 });
