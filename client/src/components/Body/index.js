0e5b6733d308cc6dead8fbfc2f0bf766b769a646
adamjorycromer <adamjorycromer@gmail.com>
2019-04-23 16:59:11 -0600

    changed layout for easier data transfer

--- client/src/components/Body/index.js
+++ client/src/components/Body/index.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { Component } from 'react';
+import Card from "../Card";
+import beetles from "../../beetles.json";
 import "./style.css";
 
-function Body(props) {
-    return <div className="body">{props.children}</div>;
+class Body extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            beetles: beetles,
+            currentScore: 0,
+            highScore: 0
+        }
+    }
+
+    render() {
+        return (
+            <div className="body">
+                <div className="score">
+                    <h1 className="float-left scoreTitle">Current Score: {this.state.currentScore}</h1>
+                    <h1 className="float-left scoreTitle">High Score: {this.state.highScore}</h1>
+                </div>
+                {this.state.beetles.map(beetle => (
+                    <Card
+                        id={beetle.id}
+                        key={beetle.id}
+                        url={beetle.url}
+                    />
+                ))}
+            </div>
+        )
+    }
 }
 
 export default Body;
