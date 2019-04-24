7faaa5e8410558c2941a8bb5a3a62c6dc1031f33
adamjorycromer <adamjorycromer@gmail.com>
2019-04-23 22:41:45 -0600

    score works

--- client/src/components/Body/index.js
+++ client/src/components/Body/index.js
@@ -10,22 +10,88 @@ class Body extends Component {
         this.state = {
             beetles: beetles,
             currentScore: 0,
-            highScore: 0
+            highScore: 0,
+            clickedArray: []
         }
+    }
+
+    shuffle = beetles => {
+        for (var i = (beetles.length - 1); i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = beetles[i];
+            beetles[i] = beetles[j];
+            beetles[j] = temp;
+        }
+        return beetles;
+    }
+
+    beetleClick = (event, id) => {
+        event.preventDefault();
+
+        let newScore = this.state.currentScore + 1;
+        if (newScore < 9) {
+            this.setState({
+                currentScore: newScore
+            });
+        }
+    }
+
+    loadScore = () => {
+        if (this.state.currentScore > 7) {
+            return this.winDiv();
+        } else {
+            return this.scoreDiv();
+        }
+    }
+
+    componentDidUpdate = () => {
+
+        if (this.state.currentScore > this.state.highScore) {
+            this.setState({
+                highScore: this.state.currentScore
+            })
+        }
+    }
+
+    reset = (event) => {
+        event.preventDefault();
+        this.setState({
+            currentScore: 0
+        })
+    }
+
+    winDiv = () => {
+        return (
+            <div className="win winOrScore">
+                <h1 className="float-left winTitle">You win!</h1>
+                <button className="float-left scoreTitle button btn btn-lg"
+                    onClick={e => this.reset(e)}>
+                    Play again?
+                </button>
+            </div>
+        )
+    }
+
+    scoreDiv = () => {
+        return (
+            <div className="score winOrScore">
+                <h1 className="float-left scoreTitle">Current Score: {this.state.currentScore}</h1>
+                <h1 className="float-left scoreTitle">High Score: {this.state.highScore}</h1>
+            </div>
+        )
     }
 
     render() {
         return (
             <div className="body">
-                <div className="score">
-                    <h1 className="float-left scoreTitle">Current Score: {this.state.currentScore}</h1>
-                    <h1 className="float-left scoreTitle">High Score: {this.state.highScore}</h1>
-                </div>
+                {this.loadScore()}
                 {this.state.beetles.map(beetle => (
                     <Card
+                        className="beetle"
                         id={beetle.id}
                         key={beetle.id}
                         url={beetle.url}
+                        clickFunction={e => this.beetleClick(e)}
                     />
                 ))}
             </div>
--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./style.css";
 
 function Card(props) {
-    return <img className="card float-left ml-3 mb-3 mt-0" alt="" src={props.url}></img>
+    return <img
+        className="card float-left ml-3 mb-3 mt-0"
+        alt=""
+        id={props.id}
+        src={props.url} 
+        onClick={props.clickFunction} />
 }
 
 export default Card;
